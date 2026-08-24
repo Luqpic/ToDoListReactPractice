78e29440de4fadd82d811f17e7053cd46f0a95e5
Luqpic <hayyanrazif@gmail.com>
2026-08-24 14:47:02 +0800

    feat: wire routes with ProtectedRoute guard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/context/AuthContext";
+import ProtectedRoute from "@/routes/ProtectedRoute";
 import TodoPage from "./pages/TodoPage";
-import { AuthProvider } from "./context/AuthContext";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <AuthProvider>
-      <TodoPage />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <TodoPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </AuthProvider>
   );
 }
